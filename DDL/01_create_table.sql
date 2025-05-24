715021d28f86a3fdc50a475f6da147cf61634ed4
Hira_Nawaz <nawazhira48@gmail.com>
2025-05-24 11:50:33 +0500

    Update 01_create_table.sql

--- DDL/01_create_table.sql
+++ DDL/01_create_table.sql
@@ -1,5 +1,9 @@
+-- 01_create_students_table.sql
+
+-- Create a Students table with basic information
 CREATE TABLE Students (
     StudentID INT PRIMARY KEY,
-    Name VARCHAR(50),
-    Age INT
+    Name VARCHAR(100),
+    Age INT,
+    Grade VARCHAR(2)
 );
