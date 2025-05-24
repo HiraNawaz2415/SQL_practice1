b475f7803287b5fe720e98b6e298d4231b72ac27
Hira_Nawaz <nawazhira48@gmail.com>
2025-05-24 12:00:31 +0500

    Create 01_insert.sql

---
+++ DML/01_insert.sql
@@ -0,0 +1,7 @@
+-- Add a student named Ali in Students table
+INSERT INTO Students (StudentID, Name, Age, Grade)
+VALUES (1, 'Ali', 20, 'A');
+
+-- Add a student named Sana in Students table
+INSERT INTO Students (StudentID, Name, Age, Grade)
+VALUES (2, 'Sana', 22, 'B');
